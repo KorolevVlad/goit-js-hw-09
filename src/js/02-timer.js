664746804fbef24f789e02bf91d443559e412a25
Korolev Vlad <korolev.v.s@bk.ru>
2022-01-14 18:16:24 +0300

    task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,41 +2,41 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const startBtn = document.querySelector('button[data-start]')
-const day =document.querySelector('span[data-days]')
-const hour = document.querySelector('span[data-hours]')
-const minute =document.querySelector('span[data-minutes]')
-const second=document.querySelector('span[data-seconds]')
+const startBtn = document.querySelector('button[data-start]');
+const day = document.querySelector('span[data-days]');
+const hour = document.querySelector('span[data-hours]');
+const minute = document.querySelector('span[data-minutes]');
+const second = document.querySelector('span[data-seconds]');
 
-startBtn.disabled=true
-let intervalId
+startBtn.disabled = true;
+let intervalId;
 
-const options = { 
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      if(selectedDates[0] < new Date()) {
-        return Notiflix.Notify.warning('Please choose a date in the future');
-      }
-        clearInterval(intervalId);
-        startBtn.disabled =false
-      function onClickStartBtn() {
-        intervalId=setInterval(()=>{
-          const deltaTime=selectedDates[0]-new Date()
-          if(deltaTime<1000){
-            clearInterval(intervalId)
-          }
-          const finishTime=convertMs(deltaTime)
-          updateClock(finishTime)
-        },1000)
-      }
-        startBtn.addEventListener('click', onClickStartBtn)
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      return Notiflix.Notify.warning('Please choose a date in the future');
     }
-};  
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    function onClickStartBtn() {
+      intervalId = setInterval(() => {
+        const deltaTime = selectedDates[0] - new Date();
+        if (deltaTime < 1000) {
+          clearInterval(intervalId);
+        }
+        const finishTime = convertMs(deltaTime);
+        updateClock(finishTime);
+      }, 1000);
+    }
+    startBtn.addEventListener('click', onClickStartBtn);
+  },
+};
 
-flatpickr("#datetime-picker", options);
+flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
   const second = 1000;
@@ -50,17 +50,15 @@ function convertMs(ms) {
   const seconds = addZeroAsSecondNumber(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
-
 }
 
 function addZeroAsSecondNumber(value) {
-    return `${value}`.padStart(2, '0');
+  return `${value}`.padStart(2, '0');
 }
 
 function updateClock({ days, hours, minutes, seconds }) {
-    day.textContent = `${days}`;
-    hour.textContent = `${hours}`;
-    minute.textContent = `${minutes}`;
-    second.textContent = `${seconds}`;
+  day.textContent = `${days}`;
+  hour.textContent = `${hours}`;
+  minute.textContent = `${minutes}`;
+  second.textContent = `${seconds}`;
 }
-
